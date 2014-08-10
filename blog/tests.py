d3442d498758a83f61441c2add57e3e01189d79b
adamiani <athos.damiani@gmail.com>
2014-08-10 02:18:44 -0300

    Post admin tests in place

--- blog/tests.py
+++ blog/tests.py
@@ -18,7 +18,6 @@ class PostTeste(TestCase):
 
         # verifica se conseguimos encontrá-lo
         todos_os_posts = Post.objects.all()
-        self.assertEqual(len(todos_os_posts), 1)
         um_post = todos_os_posts[0]
         self.assertEqual(um_post, post)
 
@@ -78,3 +77,97 @@ class AdminTest(LiveServerTestCase):
 
         # Check 'Log in' in response
         self.assertTrue('Usuário:' in response.content)
+
+    def test_create_post(self):
+        # Log in
+        self.client.login(username='teste2', password="teste2")
+
+        # Check response code
+        response = self.client.get('/admin/blog/post/add/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_create_post(self):
+        # Log in
+        self.client.login(username='teste2', password="teste2")
+
+        # Check response code
+        response = self.client.get('/admin/blog/post/add/')
+        self.assertEquals(response.status_code, 200)
+
+        # Create the new post
+        response = self.client.post('/admin/blog/post/add/', {
+            'titulo': 'My first post',
+            'texto': 'This is my first post',
+            'pub_data_0': '2013-12-28',
+            'pub_data_1': '22:00:04'
+        },
+        follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+
+        # Check added successfully
+        self.assertTrue('adicionado com sucesso' in response.content)
+
+        # Check new post now in database
+        all_posts = Post.objects.all()
+        self.assertGreater(len(all_posts), 0)
+
+    def test_edit_post(self):
+        # Create the post
+        post = Post()
+        post.titulo = 'My first post'
+        post.texto = 'This is my first blog post'
+        post.pub_data = timezone.now()
+        post.save()
+
+        # Log in
+        self.client.login(username='teste2', password="teste2")
+
+        # Edit the post
+        response = self.client.post('/admin/blog/post/1/', {
+            'titulo': 'My second post',
+            'texto': 'This is my second blog post',
+            'pub_data_0': '2013-12-28',
+            'pub_data_1': '22:00:04'
+        },
+        follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+
+        # Check changed successfully
+        self.assertTrue('modificado com sucesso' in response.content)
+
+        # Check post amended
+        todos_os_posts = Post.objects.all()
+        self.assertGreater(len(todos_os_posts), 0)
+        um_post = todos_os_posts[0]
+        self.assertEquals(um_post.titulo, 'My second post')
+        self.assertEquals(um_post.texto, 'This is my second blog post')
+
+    def test_delete_post(self):
+        # Create the post
+        post = Post()
+        post.titulo = 'My first post'
+        post.texto = 'This is my first blog post'
+        post.pub_data = timezone.now()
+        post.save()
+
+        # Check new post saved
+        todos_os_posts = Post.objects.all()
+        self.assertGreater(len(todos_os_posts), 0)
+
+        # Log in
+        self.client.login(username='teste2', password="teste2")
+
+        # Delete the post
+        response = self.client.post('/admin/blog/post/1/delete/', {
+            'post': 'yes'
+        }, follow=True)
+        self.assertEquals(response.status_code, 200)
+
+        # Check deleted successfully
+        self.assertTrue('excluído com sucesso' in response.content)
+
+        # Check post amended
+        todos_os_posts = Post.objects.all()
+        self.assertEquals(len(todos_os_posts), 0)
