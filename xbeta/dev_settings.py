48e437c6a7d145ae834579c6cc5674303cbbd3cf
adamiani <athos.damiani@gmail.com>
2014-08-17 23:51:29 -0300

    Tirei dev_settings.py do gitignore e o adicionei.

---
+++ xbeta/dev_settings.py
@@ -0,0 +1,22 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '&2v+81zuku0pffx9+se)q0jl_zaz^-gu7f0$13@q-o$0xm*y%i'
+
+# # Config do MySQL
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'xbeta',
+#         'USER': 'adamiani',
+#         'PASSWORD': 'QLg780pd',
+#         'HOST': '',
+#         'PORT': ''
+#     }
+# }
+
+# Config do PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.',
+        'NAME': '',
+    }
+}
